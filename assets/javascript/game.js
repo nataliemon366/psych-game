4132f88f8d7827cdccf49ded89ebf1d6ac1c843a
Natalie Montano <nat.montano366@gmail.com>
2019-08-30 00:07:48 -0700

    time for update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,67 @@
 alert("I ALWAYS WIN! MUAHAHA");
-var allAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p,", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"]
+var letters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p,", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"]
 
-var compPick
 var wins = 0;
 var losses = 0;
-var guessesLeft, guessedLetters, letterToGuess
+var chances = 15;
+var guessArray =[]
 
-function reset();
-function display();
+var confirmPsychics = confirm("Are you a psychic?");
 
-var random = Math.random() * allAlphabet.length
-var result = Math.floor(random)
-
-compPick = allAlphabet[result]
-{
-    return result 
-    Math.floor(Math.random()*26)
+if (confirmPsychic) {
+    alert("Is there a chance you have powers of the supernatural?")
+    alert("See if you can guess which letter the computer is thinking of.")
 }
+else {
+    alert("Wrong Answer,Try Again");
+    location.reload();
+}
+
+//set up key funcition for key press
+
 document.onkeyup = function (event) {
     console.log(event.key)
-    var userChoice = event.key
+    var userGuess = event.key;
 
-    // console.log(compPick === userChoice)
-    if (guess === LettertoGuess){
+    var computerGuess =
+    computerChoice[Math.floor(Math.random()* computerChoices.length)];
+
+    // chech 2 insure if the user is in the array of letters.
+
+
+    console.log(compPick === userChoice)
+    if (guess === LettertoGuess) {
         wins();
-    }else if (GuessesLeft - 1 === 0) {
+    } else if (GuessesLeft - 1 === 0) {
 
-   lost();
-    }else{
+        lost();
+    } else {
         fail(guess);
-    } 
+    }
     display();
 }
-function display(){
-    var winsP =document.getElementById("wins!");
-    var loosesP =document.getElementById("looses")
-    var guessesLeft =document.getElementById("guessed");
-    winsP.innerHTML
+function display() {
+    var winsP = document.getElementById("wins!");
+    var loosesP = document.getElementById("looses")
+    var guessesLeft = document.getElementById("guessed");
+    winsP.innerHTML = wins;
+    lossesP.innerHTML = loses;
+    guessesLeft.innerHTML = guessesLeft;
+    letterGuessed.innerHTML = guessedLetters.join(',')
+}
+function win() {
+    wins++;
+    resetGame();
+}
+function lost() {
+    losses++;
+    resetGame();
+}
+function reset(letter) {
+    guessesLeft = 12;
+    guessedLetters = [];
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log("Letter to Guess:" + letterToGuess);
 }
 
 
-
-console.log(compPick)
-reset()
-console.log(compPick)
-
-
-
-// console.log(reset())
