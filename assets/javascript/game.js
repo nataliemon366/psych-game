72a4a2693d143cab3920ddf179bd82d4d8fd3d63
Natalie Montano <nat.montano366@gmail.com>
2019-08-29 20:31:45 -0700

    changed some J.s

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,50 @@
-console.log("hi")
+alert("I ALWAYS WIN! MUAHAHA");
+var allAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p,", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"]
+
+var compPick
+var wins = 0;
+var losses = 0;
+var guessesLeft, guessedLetters, letterToGuess
+
+function reset();
+function display();
+
+var random = Math.random() * allAlphabet.length
+var result = Math.floor(random)
+
+compPick = allAlphabet[result]
+{
+    return result 
+    Math.floor(Math.random()*26)
+}
+document.onkeyup = function (event) {
+    console.log(event.key)
+    var userChoice = event.key
+
+    // console.log(compPick === userChoice)
+    if (guess === LettertoGuess){
+        wins();
+    }else if (GuessesLeft - 1 === 0) {
+
+   lost();
+    }else{
+        fail(guess);
+    } 
+    display();
+}
+function display(){
+    var winsP =document.getElementById("wins!");
+    var loosesP =document.getElementById("looses")
+    var guessesLeft =document.getElementById("guessed");
+    winsP.innerHTML
+}
+
+
+
+console.log(compPick)
+reset()
+console.log(compPick)
+
+
+
+// console.log(reset())
